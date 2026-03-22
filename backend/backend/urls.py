7c1f48daac62d7087027981b2d1e9833ac2db0ad
unknown <nmyumul@student.hau.edu.ph>
2026-03-22 17:51:08 +0800

    Added Backend Landing Page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,11 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({"message": "Spotlight2 API is live!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/chatbot/', include('ai_chatbot.urls')),
     path('api/', include('base.urls')),
+    path('', api_root),
 ]
 
 if settings.DEBUG:
