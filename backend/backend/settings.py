79b7005d742d21763f2a4df80b96424594418fb8
unknown <nmyumul@student.hau.edu.ph>
2026-03-22 20:04:30 +0800

    Added vercel.json

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').upper() == 'TRUE'
 
-ALLOWED_HOSTS = ['spotlight2-etmb.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['spotlight2-etmb.onrender.com', '127.0.0.1', 'localhost', '.onrender.com']
 
 
 # Application definition
